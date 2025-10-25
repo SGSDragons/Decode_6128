77f3131706a72f7e8828d8d0820b4f0cf08e7504
lbradford <lbradford@gmail.com>
2025-10-25 14:57:23 -0700

    Added an OpMode for testing with a single person

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeSinglePlayer.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.BasicOpMode.*;
+
+@TeleOp(name="6128 DECODE Singleplayer OpMode", group="OpMode")
+public class SingleplayerOpMode extends LinearOpMode{
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        final DriveTrain drive = new DriveTrain(hardwareMap);
+
+        // Wait for the game to start (driver presses START)
+        waitForStart();
+        runtime.reset();
+
+        // Set shooter wheel and belt-collector wheel motors
+        final DcMotor shooterMotor, bCMotor;
+        shooterMotor  = hardwareMap.get(DcMotor.class, "shoot");
+        bCMotor       = hardwareMap.get(DcMotor.class, "collect");
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Get input from the gamepad
+            double forward = -gamepad1.left_stick_y;  // Forward is negative Y
+            double strafe = gamepad1.left_stick_x;   // Left/Right strafe
+            double turn = gamepad1.right_stick_x;    // Turn left/right
+
+            drive.drive(forward, strafe, turn);
+
+            // Spin shooter wheel if the trigger is being held
+            if (gamepad1.right_trigger > 0) {
+                shooterMotor.setPower(1.0);
+            } else {
+                shooterMotor.setPower(0.0);
+            }
+
+            // Spin belt-collector wheel if the trigger is being held
+            if (gamepad1.left_trigger > 0) {
+                bCMotor.setPower(1.0);
+            }
+            // Backwards button for when the artifacts are in the way of the shooter
+             else if (gamepad1.left_bumper) {
+                bCMotor.setPower(-0.5);
+            } else {
+                bCMotor.setPower(0.0);
+            }
+        }
+    }
+}
